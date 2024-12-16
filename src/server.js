a5f220715c3a0caa7fb858073718599b8bbee76f
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2024-12-16 12:45:13 -0300

    documentação

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require('axios');//axios é uma biblioteca que permite fazer requisições http, com base em promises (async, await)
+const cheerio = require('cheerio');//cheerio é uma biblioteca que nos permite manipular o conteúdo html da url fornecida
 
 const url = 'https://investidor10.com.br/fiis/';
 
@@ -10,24 +10,23 @@ let fundos = [
 ];
 
 module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Permitir todas as origens
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS'); // Métodos permitidos
+    res.setHeader('Access-Control-Allow-Origin', '*'); // cabeçalho http que permite acesso de todas as origens
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS'); // define que os métodos permitidos são apenas get e options, logo não é possível alterar nada
 
-    if (req.method === 'OPTIONS') {
-        // Responder a preflight requests
+    if (req.method === 'OPTIONS') {//caso o método selecionado seja options
         res.status(200).end();
         return;
     }
 
-    if (req.method === 'GET') {
+    if (req.method === 'GET') {//caso o método selecionado seja get
         try {
-            let fundosAtualizados = await main();
-            res.status(200).json({ fundosAtualizados });
+            let fundosAtualizados = await main();//vai executar a main
+            res.status(200).json({ fundosAtualizados });//se tudo correr bem vai retornar um json com os dados 
         } catch (error) {
             console.error('Erro:', error);
             res.status(500).json({ error: 'Erro ao coletar dados' });
         }
-    } else {
+    } else {//se a requisição não for nem get nem options, retorna dizendo que a requisição não é permitida
         res.setHeader('Allow', ['GET']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
@@ -36,21 +35,24 @@ module.exports = async (req, res) => {
 async function main() {
     for (let i = 0; i < fundos.length; i++) {
         try {
-            const response = await axios.get(url + fundos[i].ticker, {
+            const response = await axios.get(url + fundos[i].ticker, {//acessa a url com um método do tipo get
                 headers: {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+                    //O cabeçalho User-Agent informa ao servidor quem está fazendo a requisição. Neste caso, está sendo enviado um valor que simula um navegador comum (Chrome), o que pode ser útil para evitar bloqueios de acesso a determinadas APIs que rejeitam requisições sem um User-Agent adequado ou que são feitas por scripts.
                 }
             });
 
-            const html = response.data;
-            const $ = cheerio.load(html);
+            const html = response.data;//armazena os dados da requisição http, que no caso é um html
+            const $ = cheerio.load(html);//lê os elementos html
+
+            //aqui é onde acessamos partes específicas do html para extrair os dados que queremos:
 
             // cotação
             $("._card.cotacao ._card-body div").each(function () {
                 fundos[i].cotacao = $(this).find(".value").text().trim();
             });
 
-            // p/pvp
+            // p/vp
             $("._card.vp ._card-body").each(function () {
                 fundos[i].pvp = $(this).find("span").text().trim();
             });
