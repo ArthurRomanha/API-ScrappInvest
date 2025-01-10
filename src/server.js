94f79e276dbfc55a1cbc659d1dc6e5fc3cdd1be4
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2025-01-10 14:57:55 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-const puppeteer = require('puppeteer');//puppeteer é uma biblioteca que acessa a internet
+const axios = require('axios');//axios é uma biblioteca que acessa a internet
 const cheerio = require('cheerio');//cheerio é uma biblioteca que nos permite manipular o conteúdo html da url fornecida
 let dados = {
-    "indicesPadrão": [
+    indicesPadrao: [
         { "indice": "Selic", "valor": "" },
         { "indice": "CDI", "valor": "" },
         { "indice": "IPCA", "valor": "" },
         { "indice": "Ibovespa", "valor": "" }
     ],
-    "fundosPadrao": [
-        { "ticker": "gare11", "cotacao": "", "pvp": "", "precoJusto": "" ,"valueDividendYeldTwelveMonths": "", "lastDividend": "" },
+    fundosPadrao: [
+        { "ticker": "gare11", "cotacao": "", "pvp": "", "precoJusto": "", "valueDividendYeldTwelveMonths": "", "lastDividend": "" },
         { "ticker": "ggrc11", "cotacao": "", "pvp": "", "precoJusto": "", "valueDividendYeldTwelveMonths": "", "lastDividend": "" },
         { "ticker": "trxf11", "cotacao": "", "pvp": "", "precoJusto": "", "valueDividendYeldTwelveMonths": "", "lastDividend": "" }
     ]
@@ -35,44 +35,52 @@ module.exports = async (req, res) => {
         }
     } else if (req.method === 'GET') {
         dadosAtt = await main(dados.fundosPadrao);
-        res.status(200).json({dadosAtt});
+        res.status(200).json({ dadosAtt });
     } else {
         res.setHeader('Allow', ['POST', 'GET']);
         res.status(405).end(`Method ${req.method} Not Allowed`); // Responde com erro 405
     }
 };
-
+async function run() {
+    let dadosAtt = await main(dados.fundosPadrao);
+    console.log(dadosAtt);
+}
 async function main(fundos) {
     try {
         url = 'https://investidor10.com.br/indices/';
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(url);
-        await page.waitForSelector('.indices-grid');
-        const html = await page.content();
+        const response = await axios(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Connection': 'keep-alive',
+                'Upgrade-Insecure-Requests': '1'
+            }
+        });
+
+        const html = response.data;
         const $ = cheerio.load(html);
         //aqui é onde acessamos partes específicas do html para extrair os dados que queremos:
-        
+
         //Selic
-        $(".indices-grid .index-card").eq(0).children(".body").each(function () {
-            dados.indicesPadrão[0].valor = $(this).find("p strong").text().trim();
+        $(".indices-grid .index-card").eq(2).children(".body").each(function () {
+            dados.indicesPadrao[0].valor = $(this).find("p strong").text().trim();
         });
 
         //CDI
-        $(".indices-grid .index-card").eq(2).children(".body").each(function () {
-            dados.indicesPadrão[1].valor = $(this).find("p strong").text().trim();
+        $(".indices-grid .index-card").eq(0).children(".body").each(function () {
+            dados.indicesPadrao[1].valor = $(this).find("p strong").text().trim();
         });
 
         //IPCA
         $(".indices-grid .index-card").eq(1).children(".body").eq(0).children("p").each(function () {
-            dados.indicesPadrão[2].valor = $(this).find("strong").text().trim();
+            dados.indicesPadrao[2].valor = $(this).find("strong").text().trim();
         });
 
         //IBOV
         $(".indices-grid .index-card").eq(3).children(".body").eq(0).children("p").each(function () {
-            dados.indicesPadrão[3].valor = $(this).find("strong").text().trim();
+            dados.indicesPadrao[3].valor = $(this).find("strong").text().trim();
         });
-        await browser.close();
     } catch (error) {
         console.error('Erro ao fazer a requisição:', error);
     }
@@ -80,11 +88,18 @@ async function main(fundos) {
     for (let fundo of fundos) {
         try {
             url = 'https://investidor10.com.br/fiis/';
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto(url+fundo.ticker);
-            await page.waitForSelector('._card.cotacao');
-            const html = await page.content();
+            const response = await axios.get(url + fundo.ticker, {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                    'Accept-Language': 'en-US,en;q=0.5',
+                    'Connection': 'keep-alive',
+                    'Upgrade-Insecure-Requests': '1'
+                }
+            });
+
+
+            const html = response.data;
             const $ = cheerio.load(html);
             //aqui é onde acessamos partes específicas do html para extrair os dados que queremos:
 
@@ -110,11 +125,10 @@ async function main(fundos) {
             $(".cell").eq(14).children(".desc").each(function () {
                 fundo.lastDividend = $(this).find(".value").text().trim();
             });
-            await browser.close();
         } catch (error) {
             console.error('Erro ao fazer a requisição:', error);
         }
     }
-
     return dados;
 }
+run();
